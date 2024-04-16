9fcca70fad6f83a0ae6d007e652e40cd95810067
donny <donnynoorlander@gmail.com>
2024-04-16 10:15:20 +0200

    iets

--- src/AdvancedAstrology.java
+++ src/AdvancedAstrology.java
@@ -20,23 +20,26 @@ public class AdvancedAstrology {
     }
 
     public static void printTriangle(int size) {
-        for (int i = 1; i < size; i++){
+        for (int i = 1; i < size; i++) {
             printSpaces(size - i);
             printStars(i);
         }
     }
 
     public static void christmasTree(int height) {
-        // part 3 of the exercise
+        for (int i = 1; i < height; i++) {
+            printSpaces(height - i);
+            printStars(i);
+        }
     }
 
-    public static void main(String[] args) {
-        // The tests are not checking the main, so you can modify it freely.
+        public static void main (String[]args){
+            // The tests are not checking the main, so you can modify it freely.
 
-        printTriangle(5);
-        System.out.println("---");
-        christmasTree(4);
-        System.out.println("---");
-        christmasTree(10);
+            printTriangle(5);
+            System.out.println("---");
+            christmasTree(4);
+            System.out.println("---");
+            christmasTree(10);
+        }
     }
-}
