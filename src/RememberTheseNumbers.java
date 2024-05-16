a90a5a61e612aca5f9e01ecc0508bcf9b7f2cdeb
armando <donnynoorlander@gmail.com>
2024-05-16 14:52:49 +0200

    Iets

--- src/RememberTheseNumbers.java
+++ src/RememberTheseNumbers.java
@@ -1,7 +1,8 @@
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class SecondPlusThird {
+public class RememberTheseNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -9,15 +10,14 @@ public class SecondPlusThird {
         ArrayList<Integer> numbers = new ArrayList<>();
         while (true) {
             int number = Integer.valueOf(scanner.nextLine());
-            if (number == 0) {
+            if (number == -1) {
+             numbers.forEach(System.out::println);
                 break;
             }
 
+
+
             numbers.add(number);
         }
-
-        System.out.println(numbers.get(1));
-        System.out.println(numbers.get(2));
     }
 }
-
