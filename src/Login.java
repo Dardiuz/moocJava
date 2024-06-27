309c5d38e2240326e5df8cf5a839379c0bcf1a6f
donny <donnynoorlander@gmail.com>
2024-06-27 14:28:30 +0200

    moet afgemaakt

---
+++ src/Login.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Login {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String username1 = "emma";
+        String username2 = "alex";
+
+        String password1 = "sunshine";
+        String password2 = "haskell";
+
+        System.out.println("Enter username: ");
+        String input1 = scanner.nextLine();
+
+        System.out.println("Enter password: ");
+        String input2 = scanner.nextLine();
+
+    }
+}
